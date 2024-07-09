be215106dd9e9500aa93fb7dadf86c727ab15eda
avishag-gvili <b3206003@gmail.com>
2024-07-09 12:16:00 +0300

    1

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+console.log("1")
 test('renders learn react link', () => {
-  expect(true).toEqual(false)
+  // expect(true).toEqual(false)
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
